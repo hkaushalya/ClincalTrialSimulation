a4df5cab5dab0487c43854b1b3078c3101b51195
hews <sam.hewamanage@tessella.com>
2015-03-03 16:39:01 -0500

    Basic simulator. Need to add realistic beaviours to Patients Dropping out of the trial and Dose Limiting Toxicity. Not sure what kind of parameters to be used in building the DLT model.

--- rawcode/simulator.R
+++ rawcode/simulator.R
@@ -1,34 +1,108 @@
 rm(list=ls())
 
-N <- 20  # Number of patients in the trail
-pop <- seq(1, N)
+set.seed(1234)
+gN <- 100  # Number of patients in the trail
+gPOP <- seq(1, gN)
+gSAMPLE_SIZE <- 3
+gDOSE_LEVELS <- seq(25,500, 50)
+gMIN_DOSE_SEARCH_STEP <- 5   # this will be minimal dose level seperation
+gDOSE_BREAKS <- 4                  # number of dose intervals for a given dose range
 
+#When a DLT is found, refine the dose level to find a better MTD
+# Here maxDose will be the DLT for and minDose will be the MTD (dose before DLT)
+RefineDose <- function(minDose, maxDose)
+{
+    #ToDo: Want to make this int
+    #This could be some function. For now lets split to constant intervals
+    di <- (maxDose - minDose) / gDOSE_BREAKS
+    doses <- seq(from=minDose, to=maxDose, by=floor(di))
+    
+    return (doses)
+}
 
-RandomGroup<-function(n, pop) {
+#Pick a random group from pop without replacement
+RandomGroup<-function(sampleSize, pop) {
   grp <- c()
 
-  if (length(pop)<n) {
+  if (length(pop) < sampleSize) {
     grp = sample(pop, length(pop), replace = F)  
   } else
   {
-    grp = sample(pop, size=n, replace =F)
+    grp = sample(pop, size=sampleSize, replace =F)
+  }
+  return (grp)
+}
+
+#### Patient Drop Out Simulator
+Dropout <- function()
+{
+  # How do we make this random
+  #lambda <- 1
+  #p <- dpois(1, lambda)
+  
+  drop_prob <- runif(1)
+  print(paste0("drop_prob=", drop_prob))
+  if (drop_prob>0.8)  {
+    return TRUE
+  } else {
+    return FALSE
+  }
+}
+
+Simulate<- function(Npat, doseLevels)
+{
+
+  if (missing(Npat)) {
+    print(paste0("Simulate: input Npat not found" ))
+  }
+  if (missing(doseLevels)) {
+    print(paste0("Simulate: input doseLevels not found" ))
   }
   
-  # remove the selected people from population
-  cat (pop)
-  print("\n")
-  cat(grp)
-  print("\n")
+  DLT <- 0
+  MTD <- 0
   
-  return (grp)
-  #msg <- paste0("selected:", grp , " remaining pop:", cat(pop), "\n")
-  #print(msg)
+  for (dl in 1:length(doseLevels)) {
+    grp <- RandomGroup(gSAMPLE_SIZE, Npat)
+    Npat <- setdiff(Npat, grp)
+    
+    #simulate drop outs with very low prob
+    if (Dropout) {
+      grp_plus <- RandomGroup(gSAMPLE_SIZE, Npat)
+      Npat <- setdiff(Npat, grp_plus)
+      grp <- grp[1:(length(g)-1)]  # drop the last patiend in this list for now
+      grp <- c(grp, grp_plus)       # new list of patients taking the current dose
+      print(paste0("A patient drops out simulated."))
+    }
+
+    dltProbs <- runif(length(grp))
+    print (paste0("========== dl=",dl, "=============="))
+    print(paste0("Npat=", length(Npat)))
+    print(dltProbs)
+    
+    
+    if ( sum(dltProbs> 0.65) >= 2) { #2/3 pateints show toxic levels and DLT reached (0.8 is arbitray for now)
+      # MTD is reached
+      DLT <- doseLevels[dl]
+      MTD <- DLT
+      if ( (dl-1) != 1) {
+        MTD <- doseLevels[dl-1]
+      }
+      print("\n")
+      print (paste0("DLT Reached@MTD/DLT:", MTD, "/",DLT))
+      return (DLT) 
+    
+      } else {
+        MTD <- doseLevels[dl]
+      }
+  } # for
+  
+  # If no DLT reached in the loop, means no DLT condition found
+  print("No DLT conditions found! Last dose tested was:")
+  print(paste0("MTD=", MTD, " DLT=", DLT))
+  
 }
 
+################### Main Routine
 
-for (i in 1:10) {
-  print(paste0("i === " , i))
-  grp <- RandomGroup(3, pop)
-  pop <- setdiff(pop,grp)
-  
-}
+Simulate(gPOP, gDOSE_LEVELS)
